3662844824e9b73ac74c999fa200109e09e4bd57
RCosson <romain.cosson11@live.fr>
2018-01-28 08:41:09 +0100

    creation templates a propos et contact ainsi que leur action dans le controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,10 +12,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller {
 
 	/**
-	* @Route("/")
+	* @Route("/", name="homepage")
 	*/
     public function indexAction() {
         
         return $this->render('rc/index.html.twig');
+    }
+
+    /**
+	* @Route("/about", name="about")
+	*/
+    public function aboutAction() {
+        
+        return $this->render('rc/about.html.twig');
+    }
+
+    /**
+	* @Route("/contact", name="contact")
+	*/
+    public function contactAction() {
+        
+        return $this->render('rc/contact.html.twig');
     }
 }
